f498c728923d59619bc4e9215915249f287bc830
Андрей Алексеев <andrewalexeev@mvideo.ru>
2020-04-15 00:30:22 +0300

    disable cors

--- src/main/java/com/mai/projects/plm/security/config/SecurityConfig.java
+++ src/main/java/com/mai/projects/plm/security/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .httpBasic().and()
+                .cors().disable()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
